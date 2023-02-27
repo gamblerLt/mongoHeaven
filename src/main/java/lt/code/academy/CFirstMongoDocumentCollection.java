5a78e71b2afca53377b33457e6a48805fa4238fa
gamblerLt <rimas.spakauskas@outlook.com>
2023-02-27 18:39:39 +0200

    MongClient

---
+++ src/main/java/lt/code/academy/CFirstMongoDocumentCollection.java
@@ -0,0 +1,18 @@
+package lt.code.academy;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import lt.code.academy.client.MongoClientProvider;
+import org.bson.Document;
+
+public class CFirstMongoDocumentCollection {
+    public static void main(String[] args) {
+        //1 step
+        MongoClient client = (MongoClient) MongoClientProvider.getClient();
+        // 2 step
+        MongoDatabase database = client.getDatabase("myMongoDB");
+        //3 step
+        MongoCollection<Document> users = database.getCollection("users");
+    }
+}
