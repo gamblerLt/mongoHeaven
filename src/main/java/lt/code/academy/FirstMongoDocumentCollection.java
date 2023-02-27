ed184959219685a95cf77bbb7b0d94f595393578
gamblerLt <rimas.spakauskas@outlook.com>
2023-02-27 18:47:52 +0200

    MongClient

--- src/main/java/lt/code/academy/FirstMongoDocumentCollection.java
+++ src/main/java/lt/code/academy/FirstMongoDocumentCollection.java
@@ -6,6 +6,8 @@ import com.mongodb.client.MongoDatabase;
 import lt.code.academy.client.MongoClientProvider;
 import org.bson.Document;
 
+import java.util.List;
+
 public class FirstMongoDocumentCollection {
     public static void main(String[] args) {
         //1 step
@@ -14,5 +16,14 @@ public class FirstMongoDocumentCollection {
         MongoDatabase database = client.getDatabase("myMongoDB");
         //3 step
         MongoCollection<Document> users = database.getCollection("users");
+
+        Document document= new Document("name", "Anatolijus")
+                .append("age", 25)
+                        .append("phone", "+3706658558");
+
+        users.insertOne(document);
+       // users.insertMany(List.of(document, document2, document3, document4));
+
+
     }
 }
